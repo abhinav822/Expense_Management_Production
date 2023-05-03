b14c9e7737f08588afece7ba20eead9f3a30e8d0
abhinav822 <pabhinav400@gmail.com>
2023-05-03 19:30:20 +0530

    .

---
+++ client/build/precache-manifest.46c200be05013b52cc01d905509000b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5a52932b9d9ef06333f",
+    "url": "/static/css/main.67147a74.chunk.css"
+  },
+  {
+    "revision": "f5a52932b9d9ef06333f",
+    "url": "/static/js/main.f5a52932.chunk.js"
+  },
+  {
+    "revision": "d092bc3ea39a55206c5b",
+    "url": "/static/css/1.f333eff8.chunk.css"
+  },
+  {
+    "revision": "d092bc3ea39a55206c5b",
+    "url": "/static/js/1.d092bc3e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f98f67af5ccac9d998afbb75d6e271e",
+    "url": "/index.html"
+  }
+];
